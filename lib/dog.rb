86b4f5653d9ec382d6e0cdf6ea9d7497ca7cfbca
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-01 01:17:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 class Dog
-     def initialize(name,)
-       @name=name
+     def name=(name_of_dog)
+       @name=name_of_dog
+     end
+     
+     def name
+       @name
+     end
+     
+     def breed=(breed)
+       @breed=
 end
        
        
