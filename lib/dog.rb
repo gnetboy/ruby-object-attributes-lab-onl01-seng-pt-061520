606ba4f73487bcde3820ce0b17d65f5812ccf7c7
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-01 01:03:15 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+     def name = (dog_name)
+       @this_dog_name=dog_name
+     end
+     
+     def name
+       @this_dog_name
+     end
+     
+     def breed =(dog_breed)
+       @breed=dog_breed
+     end
+     
+     def breed
+       @breed
+     end
+     end
